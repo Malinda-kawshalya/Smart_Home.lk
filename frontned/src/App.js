4be26ea7f1246d3289a6374e1885eac3358a11c4
Malinda Kawshalya <malindakawshalya@gmail.com>
2025-05-22 21:32:34 +0530

    update

--- frontned/src/App.js
+++ frontned/src/App.js
@@ -8,14 +8,7 @@ import Sidebar from './components/Sidebar';
 
 // Import pages
 import Dashboard from './pages/Dashboard';
-import Products from './pages/Products';
-import Inventory from './pages/Inventory';
-import Suppliers from './pages/Suppliers';
-import Customers from './pages/Customers';
-import Orders from './pages/Orders';
-import Delivery from './pages/Delivery';
-import Support from './pages/Support';
-import Reports from './pages/Reports';
+
 
 function App() {
   return (
@@ -26,14 +19,6 @@ function App() {
         <main className="content p-4">
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/inventory" element={<Inventory />} />
-            <Route path="/suppliers" element={<Suppliers />} />
-            <Route path="/customers" element={<Customers />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/delivery" element={<Delivery />} />
-            <Route path="/support" element={<Support />} />
-            <Route path="/reports" element={<Reports />} />
           </Routes>
         </main>
       </div>
